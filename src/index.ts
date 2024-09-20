89537a9b836359a6b97bb4f191a6fb4822e0829c
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-09-20 14:36:02 +0200

    building

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { Alumno } from "./Alumno";
 import { Materia } from "./Materia";
 import * as readline from 'readline-sync';
 
-console.log('Bienvenido al Gestor de Alumnos');
+console.log('Bienvenido al Gestor de Alumnos\nOpciones:\n1º Crear Alumno\n2º Asignar Materia');
+console.log('3º Listar Materias Alumno\n4º Crear Materia\n5º Listar Materias');
+console.log('6º Asignar nota a Materia de un Alumno\n7º Listar Alumnos con nota');
 const opc: number = readline.questionInt('Que desea hacer?: ');
